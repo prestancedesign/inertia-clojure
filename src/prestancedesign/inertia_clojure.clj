921dae5864f151e2de606a8f4148e647f953b5be
Michael SALIHI <admin@prestance-informatique.fr>
2021-04-20 01:52:38 +0200

    Add asset versionning check and http 409 response

--- src/prestancedesign/inertia_clojure.clj
+++ src/prestancedesign/inertia_clojure.clj
@@ -15,14 +15,20 @@
    (fn [request]
      (let [response (handler request)
            inertia-header (rr/get-header request "x-inertia")
-           inertia-data (-> response
-                            :inertia
-                            (update :props merge share-props))
-           url (:uri request)
-           data-page (assoc inertia-data :url url :version asset-version)]
-       (cond (= 302 (:status response)) response
-             inertia-header {:status 200
-                             :headers {"x-inertia" "true"
-                                       "x-inertia-Version" asset-version}
-                             :body data-page}
-             :else (rr/response (template (json/write-str data-page))))))))
+           inertia-version (rr/get-header request "x-inertia-version")
+           method (:request-method request)
+           url (:uri request)]
+       (if (and inertia-header (= method :get) (not= inertia-version asset-version))
+         (-> response
+             (rr/status 409)
+             (rr/header "x-inertia-location" url))
+         (let [inertia-data (-> response
+                                :inertia
+                                (update :props merge share-props))
+               data-page (assoc inertia-data :url url :version asset-version)]
+           (cond (= 302 (:status response)) response
+                 inertia-header {:status 200
+                                 :headers {"x-inertia" "true"
+                                           "x-inertia-Version" asset-version}
+                                 :body data-page}
+                 :else (rr/response (template (json/write-str data-page))))))))))
