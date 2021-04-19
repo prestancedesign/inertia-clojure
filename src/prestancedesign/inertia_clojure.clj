ca5e3534b26c5a7345bc22bb759d5fd69f255f34
Michael SALIHI <admin@prestance-informatique.fr>
2021-04-19 22:23:41 +0200

    First iteration: working render function and middleware

--- src/prestancedesign/inertia_clojure.clj
+++ src/prestancedesign/inertia_clojure.clj
@@ -1,6 +1,28 @@
-(ns prestancedesign.inertia-clojure)
+(ns prestancedesign.inertia-clojure
+  (:require [clojure.data.json :as json]
+            [ring.util.response :as rr]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (prn x "Hello, World!"))
+(defn render
+  [component props]
+  (fn [request]
+    (assoc request :inertia {:component component
+                             :props props})))
+
+(defn wrap-inertia
+  ([handler template asset-version]
+   (wrap-inertia handler template asset-version {}))
+  ([handler template asset-version share-props]
+   (fn [request]
+     (let [response (handler request)
+           inertia-header (rr/get-header request "x-inertia")
+           inertia-data (-> response
+                            :inertia
+                            (update :props merge share-props))
+           url (:uri request)
+           data-page (assoc inertia-data :url url :version asset-version)]
+       (cond (= 302 (:status response)) response
+             inertia-header {:status 200
+                             :headers {"x-inertia" "true"
+                                       "x-inertia-Version" asset-version}
+                             :body data-page}
+             :else (rr/response (template (json/write-str data-page))))))))
