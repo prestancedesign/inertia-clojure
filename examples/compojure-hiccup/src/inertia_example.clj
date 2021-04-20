7672d6e727a0e5ec32aadb82b631ad1d72baa8aa
Michael SALIHI <admin@prestance-informatique.fr>
2021-04-20 23:03:17 +0200

    Add back-end examples

---
+++ examples/compojure-hiccup/src/inertia_example.clj
@@ -0,0 +1,32 @@
+(ns inertia-example
+  (:require [compojure.core :refer [defroutes GET]]
+            [hiccup.page :as page]
+            [prestancedesign.inertia-clojure :as inertia]
+            [ring.adapter.jetty :as http]
+            [ring.middleware.json :refer [wrap-json-response]]))
+
+(def asset-version "1")
+
+(defn template [data-page]
+  (page/html5
+   [:head
+    [:meta {:charset "utf-8"}]
+    [:title "Inertia + Clojure example"]]
+   [:body
+    [:div {:id "app"
+           :data-page data-page}]
+    (page/include-js "/js/app.js")]))
+
+(defroutes routes
+  (GET "/" [] (inertia/render "index" {:name    "Superman"
+                                       :passion ["guitar" "computing"]
+                                       :sport   ["Parkour" "MMA"]}))
+  (GET "/demo" [] (inertia/render "demo" {:name "Superman"})))
+
+(def app
+  (-> routes
+      (inertia/wrap-inertia template asset-version)
+      wrap-json-response))
+
+(defn -main []
+  (http/run-jetty #'app {:port 3000 :join? false}))
---
+++ examples/reitit-selmer/src/inertia_example.clj
@@ -0,0 +1,28 @@
+(ns inertia-example
+  (:require [muuntaja.core :as m]
+            [prestancedesign.inertia-clojure :as inertia]
+            [reitit.ring :as ring]
+            [reitit.ring.middleware.muuntaja :as muuntaja]
+            [ring.adapter.jetty :as http]
+            [selmer.parser :as html]))
+
+(def asset-version "1")
+
+(defn template [data-page]
+  (html/render-file "templates/base.html" {:page data-page}))
+
+(def app
+  (ring/ring-handler
+   (ring/router
+    [["/" (inertia/render "index"
+                          {:name "Superman"})]
+     ["/demo" (inertia/render "demo"
+                              {:name "Batman"})]]
+    {:data {:muuntaja   m/instance
+            :middleware [muuntaja/format-middleware
+                         [inertia/wrap-inertia template asset-version]]}})
+   (ring/create-resource-handler {:path "/"
+                                  :root "public"})))
+
+(defn -main []
+  (http/run-jetty #'app {:port 3000 :join? false}))
