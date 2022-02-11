c7b0bdcc53546200639dd1b928dc7fed50b2107f
Michael Salihi <admin@prestance-informatique.fr>
2022-02-11 23:02:25 +0100

    SSR back end setup for compojure/hiccup example

--- examples/server-side/compojure-hiccup/src/inertia_example.clj
+++ examples/server-side/compojure-hiccup/src/inertia_example.clj
@@ -2,20 +2,30 @@
   (:require [compojure.core :refer [defroutes GET]]
             [compojure.route :as route]
             [hiccup.page :as page]
+            [hiccup.util :as h]
             [inertia.middleware :as inertia]
-            [ring.adapter.jetty :as http]))
+            [org.httpkit.client :as http]
+            [org.httpkit.server :as server]
+            [jsonista.core :as json]))
 
 (def asset-version "1")
 
 (defn template [data-page]
-  (page/html5
-   [:head
-    [:meta {:charset "utf-8"}]
-    [:title "Inertia + Clojure example"]]
-   [:body
-    [:div {:id "app"
-           :data-page data-page}]
-    (page/include-js "/js/app.js")]))
+  (let [render @(http/post "http://127.0.0.1:13714/render" {:body data-page
+                                                            :headers {"Content-Type" "application/json"}})
+        {:keys [body head]} (json/read-value (:body render) json/keyword-keys-object-mapper)]
+    (page/html5
+     [:head
+      [:meta {:charset "utf-8"}]
+      [:title "Inertia + Clojure example"]
+      (for [h head]
+        (h/as-str h))]
+     [:body
+      (if (seq body)
+        body
+        [:div {:id "app"
+               :data-page data-page}])
+      (page/include-js "/js/app.js")])))
 
 (defroutes routes
   (GET "/" [] (inertia/render "index" {:title "Hello World!"}))
@@ -26,4 +36,4 @@
 (def app (inertia/wrap-inertia routes template asset-version))
 
 (defn -main []
-  (http/run-jetty #'app {:port 3000 :join? false}))
+  (server/run-server #'app {:port 3000}))
